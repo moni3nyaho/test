fa52c97979bf56a3eda606135024b11afbd09e53
moni3nyaho <monimaru3@outlook.jp>
2025-10-15 21:18:39 +0900

    resources~storage

---
+++ storage/framework/views/9b86454c638342508696ced8d4c68dcd4bba302c.php
@@ -0,0 +1,28 @@
+
+
+<?php $__env->startSection('content'); ?>
+<div class="row">
+  <div class="col-9">
+    <div class="container mt-4">
+      <div class="row w-100">
+        <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <div class="col-3">
+          <a href="<?php echo e(route('products.show', $product)); ?>">
+            <img src="<?php echo e(asset('img/dummy.png')); ?>" class="img-thumbnail">
+          </a>
+          <div class="row">
+            <div class="col-12">
+              <p class="samuraimart-product-label mt-2">
+                <?php echo e($product->name); ?><br>
+                <label>¥<?php echo e($product->price); ?></label>
+              </p>
+            </div>
+          </div>
+        </div>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+      </div>
+    </div>
+  </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel-samuraimart\resources\views/products/index.blade.php ENDPATH**/ ?>
